2eeaa36dcd7965df6d2e0d2d5a4416f6f1c317f0
Antony Iorio <antony.iorio@paradyn.com.au>
2025-05-02 21:54:58 +0900

    make sure meta data is enabled

--- engine/const.go
+++ engine/const.go
@@ -3,7 +3,7 @@ package engine
 import "fmt"
 
 var (
-	LabelPrefix = "wp.autoscaler/"
+	LabelPrefix = "wp.autoscaler."
 	LabelPool   = fmt.Sprintf("%spool", LabelPrefix)
 	LabelImage  = fmt.Sprintf("%simage", LabelPrefix)
 
