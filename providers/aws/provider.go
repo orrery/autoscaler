9ac52940239b0923b3370011e61695f2ef59172e
Antony Iorio <antony.iorio@paradyn.com.au>
2025-05-02 21:27:25 +0900

    make sure meta data is enabled

--- providers/aws/provider.go
+++ providers/aws/provider.go
@@ -100,10 +100,10 @@ func (p *Provider) DeployAgent(ctx context.Context, agent *woodpecker.Agent) err
 		ImageId:      aws.String(p.amiID),
 		InstanceType: types.InstanceType(p.instanceType),
 		MetadataOptions: &types.InstanceMetadataOptionsRequest{
-			HttpEndpoint:            InstanceMetadataEndpointStateEnabled,
+			HttpEndpoint:            types.InstanceMetadataEndpointStateEnabled,
 			HttpPutResponseHopLimit: aws.Int32(1),
-			HttpTokens:              HttpTokensStateRequired,
-			InstanceMetadataTags:    InstanceMetadataTagsStateEnabled,
+			HttpTokens:              types.HttpTokensStateRequired,
+			InstanceMetadataTags:    types.InstanceMetadataTagsStateEnabled,
 		},
 		SecurityGroupIds: p.securityGroups,
 		MinCount:         aws.Int32(1),
